dcef9f7ade76faa6feb4d603b1e35ac72d6c7f3e
PintoDrop <mikebender22@gmail.com>
2023-02-22 09:54:55 -0800

    added console commands

--- script.js
+++ script.js
@@ -17,36 +17,30 @@ for (var i = 0; i < = passwordLength; i++) {
 
     document.getElementById("generate").value = password;
   }
-  
-  var passwordLength;
-  var confirmLower;
-  var confirmUpper;
-  var confirmNumber;
-  var confirmSpecial;
-  var userChoices;
 
   var length = parseInt(prompt('Desired Length'));
   */
  
  function generate(){
-   passwordLength = prompt("How many characters would you like the password to be? Choose between 8 & 128")
+   passwordLength = parseInt(prompt("How many characters would you like the password to be? Choose between 8 & 128"))
    console.log("Passowrd length" + passwordLength);
 
    if (!passwordLength) {
      alert("Required value");
    } else if (passwordLength < 8 || passwordLength > 128);
    
-   var numbers = confirm('Numbers');
+   var numbers = confirm('Would you like to include numbers?');
    console.log("Number " + confirmNumber);
 
-   var uppercase = confirm('Uppercase');
+   var uppercase = confirm('Would you like to include uppercase letters?');
    console.log("Uppercase" + confirmUpper);
 
-   var lowercase = confirm('Lowercase');
+   var lowercase = confirm("Would you like to include lowercase letters?");
    console.log("Lowercase" + confirmLower);
 
-   var symbols = confirm('Symbols');
+   var symbols = confirm('Would you like to include special characters?');
    console.log("Symbols" + confirmSpecial);
+   console.log(passwordLength, numbers, uppercase, lowercase, symbols)
 
   
   var passwordOptions = {
@@ -116,31 +110,28 @@ function generatePassword() {
       passwordArray.push(character);
     }
   }
-
   return passwordArray.join("");
 }
   
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 var passwordLength;
-var confirmLower;
+var confirmLower; 
 var confirmUpper;
-var confirmNumber;
+var confirmNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var confirmSpecial;
 var userChoices;
 
 var lowercase = ["a","b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 var blankUpper = [];
 var toUpper = function (x) {
   return x.toUpper();
 };
-
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 // Write password to the #password input
 function writePassword() {
